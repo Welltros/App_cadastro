7781dc3e9f2b51e2453ded89a4ce1f56bc799e7e
welltros <wellmoreiradasilva@gmail.com>
2024-09-17 19:41:58 -0300

    CRUD

--- view.py
+++ view.py
@@ -3,25 +3,55 @@ import sqlite3 as lite
 #Criando conexão  com o banco
 con = lite.connect('dados.db')
 
-dados = ['vaso','sala de estar', 'vaso que comprei ao lado','Marca chinesa','27/08/2024','100','seriexxccsx','c:imagens']
+#CRUD
+
 #Inserir dado no banco
-with con:
-  cur = con.cursor()
-  query = "INSERT INTO inventario(nome,local, descricao, marca, data_da_compra,valor_da_compra, serie, imagem) VALUES(?,?,?,?,?,?,?,?)"
-  cur.execute(query,dados)    #Executa a query que salva os valores no banco de dados na tabela iventario
+def inserir_form(i):
+    with con:
+    cur = con.cursor()
+    query = "INSERT INTO inventario(nome,local, descricao, marca, data_da_compra,valor_da_compra, serie, imagem) VALUES(?,?,?,?,?,?,?,?)"
+    cur.execute(query,i)    #Executa a query que salva os valores no banco de dados na tabela iventario
 
-ver_dados =[]
+atualizar_dados = ['vaso','cozinha', 'vaso que comprei ao lado','Marca chinesa','27/08/2024','100','seriexxccsx','c:imagens',1]
+
+#Atualizar dado no banco
+def atualizar_form(i):
+  with con:
+    cur = con.cursor()
+    query = "UPDATE inventario SET nome=?,local=?, descricao=?, marca=?, data_da_compra=?,valor_da_compra=?, serie=?, imagem=? WHERE id=? "
+    cur.execute(query,i)   
+
+#Apagar dados no banco
+def deletar_form(i):
+  with con:
+    cur = con.cursor()
+    query = "DELETE FROM inventario WHERE id=?" 
+    cur.execute(query,i)
+
 #Ver dados no banco
-with con:
-  cur = con.cursor()
-  query = "SELECT * FROM inventario" #Seleciona tudo da tabela inventario
-  cur.execute(query)
+def ver_form():
+  ver_dados =[]
+  with con:
+    cur = con.cursor()
+    query = "SELECT * FROM inventario" #Seleciona tudo da tabela inventario
+    cur.execute(query)
 
-  rows = cur.fetchall()
-  for row in rows:
-    ver_dados.append(row)
-print(ver_dados)
+    rows = cur.fetchall()
+    for row in rows:
+      ver_dados.append(row)
+  return ver_dados
 
 
 
-'''parei no curso 2 aos 20minutos link https://www.youtube.com/watch?v=dV0B6xWsbJE'''
+#Ver dados no banco
+def ver-item(id):
+  ver_dados_inidividual =[]
+  with con:
+    cur = con.cursor()
+    query = "SELECT * FROM inventario Where id=?" 
+    cur.execute(query,id)
+
+    rows = cur.fetchall()
+    for row in rows:
+      ver_dados_inidividual.append(row)
+  print(ver_dados)
